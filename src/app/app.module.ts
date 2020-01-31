37c0e0ce91c6433bc8aa5afebdc0d838b757b698
Dzianis.Lukayenka <Dzianis.Lukayenka@thomsonreuters.com>
2020-01-31 17:55:59 +0300

    feat(login-page): implement a simple page markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { CoreModule } from './core/core.module';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 import { SharedModule } from './shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesPageComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
